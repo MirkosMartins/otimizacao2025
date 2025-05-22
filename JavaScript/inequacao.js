f3482119d7da217a6bd74bb44b041fc0ac024177
Pedro Balen <p.balen99@gmail.com>
2025-05-22 16:22:49 -0300

    Update inequacao.js

--- JavaScript/inequacao.js
+++ JavaScript/inequacao.js
@@ -1,65 +1,41 @@
 function inequacoes(f, p, tipo) {
-    const aux = {
-        maior: p.y > (f.a * p.x + f.b),  
-        menor: p.y < (f.a * p.x + f.b),  
-        igual: p.y === (f.a * p.x + f.b), 
-    };
+  const yFunc = f.a * p.x + f.b;
+  const aux = {
+    menor: p.y < yFunc,
+    menor_igual: p.y <= yFunc,
+    maior: p.y > yFunc,
+    maior_igual: p.y >= yFunc,
+    igual: p.y === yFunc,
+  };
 
-    if (tipo === '<') {
-        return aux.menor;  
-    } else if (tipo === '<=') {
-        return aux.menor || aux.igual;  
-    } else if (tipo === '>') {
-        return aux.maior;  
-    } else if (tipo === '>=') {
-        return aux.maior || aux.igual;  
-    } else if (tipo === '=') {
-        return aux.igual;  
-    } else {
-        return 'erro';  
-    }
+  switch (tipo) {
+    case "<":
+      return aux.menor;
+    case "<=":
+      return aux.menor_igual;
+    case ">":
+      return aux.maior;
+    case ">=":
+      return aux.maior_igual;
+    default:
+      return false;
+  }
 }
 
+const func = { a: 2, b: 1 };
+const sinal = ">=";
 
-const retas = { 
-    p1: { x: 2, y: 4 }, 
-    p2: { x: 5, y: 2 } 
-};
-const ang = (retas.p2.y - retas.p1.y) / (retas.p2.x - retas.p1.x);
-retas.f = { a: ang, b: (retas.p1.y - ang * retas.p1.x) };
+const pontos = [
+  { x: 2, y: 6 },
+  { x: -3, y: 4 },
+  { x: -2, y: 3 },
+  { x: 4, y: -2 },
+];
 
+console.log(`Equação da reta: y = ${func.a}x + ${func.b}`);
+console.log(`Inequação: y ${sinal} ${func.a}x + ${func.b}\n`);
 
-let pontos = [{ x: 4, y: 5 }];
-const quant = Math.floor(Math.random() * (20 - 4 + 1)) + 4;
-
-
-/*const pontos = [
-    { x: 1, y: 3 },   
-    { x: 3, y: 3 },   
-    { x: 4, y: 1 },   
-    { x: 6, y: 0 },   
-]; 
-
-pontos.forEach(ponto => {
-    ops.forEach(op => {
-        const resultado = inequacoes(retas.f, ponto, op);
-        console.log(`Ponto (${ponto.x}, ${ponto.y}) | Inequação '${op}' | Resultado: ${resultado}`);
-    });
-});*/
-
-const op = '>';  
-
-
-for (let i = pontos.length; i < quant; i++) {
-    pontos.push({ 
-        x: Math.floor(Math.random() * (100 - (-100) + 1)) + (-100), 
-        y: Math.floor(Math.random() * (100 - (-100) + 1)) + (-100) 
-    });
-}
-
-console.log(`y ${op} ${retas.f.a} x + (${retas.f.b})`);
-
-for (let i = 0; i < quant; i++) {
-    const resultado = inequacoes(retas.f, pontos[i], op);
-    console.log(`(${pontos[i].x}, ${pontos[i].y}) : ${resultado}`);
-}
+pontos.forEach((p) => {
+  const res = inequacoes(func, p, sinal);
+  console.log(`Ponto (${p.x}, ${p.y}) -> ${res ? "Satisfaz" : "Não satisfaz"}`);
+});
